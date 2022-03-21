937ae775e3a086be401795c89b384b9ee664bd6b
carlosrado <43089893+carlosrado@users.noreply.github.com>
2022-03-21 10:01:15 +0100

    correccion provide

---
+++ provide_map/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+import os #incluir
+from glob import glob #incluir
+
+package_name = 'provide_map'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'map'), glob('map/*.pgm')),#incluir
+        (os.path.join('share', package_name, 'map'), glob('map/*.yaml')),#incluir
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),#incluir
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),#incluir
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='carlos',
+    maintainer_email='43089893+carlosrado@users.noreply.github.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
